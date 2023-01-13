1a912c6e52a6ef8e75e30c3f0380684baebdef40
Melony Franchini <meltravelz@gmail.com>
2023-01-13 09:32:39 -0700

    test: Add exists & attributes

---
+++ spec/budget_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/employee'
+require './lib/department'
+require './lib/budget'
+
+RSpec.describe Budget do
+
+  it 'exists' do
+    budget = Budget.new("2022")   
+
+    expect(budget).to be_instance_of(Budget)
+  end
+
+  it "has attributes" do
+    budget = Budget.new(year)   
+
+    expect(budget.year).to eq(2022)
+  end
+
+end
