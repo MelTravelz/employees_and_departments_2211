4ac3c9593604378c014c646463978c6825eac649
Melony Franchini <meltravelz@gmail.com>
2023-01-13 09:25:45 -0700

    test: Add #expense

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe Department do
     expect(customer_service.employees).to eq([bobbi, aaron])
   end
 
-  it "can #hire new emplyees" do
+  it "can have #expenses" do
     customer_service = Department.new("Customer Service") 
     
     bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
@@ -39,6 +39,21 @@ RSpec.describe Department do
     customer_service.hire(aaron) 
 
     expect(customer_service.expenses).to eq(0)
+  end
+
+  it "can add an #expense to it's expenses" do
+    customer_service = Department.new("Customer Service") 
+    
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})  
+
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron) 
+
+    customer_service.expense(100)
+    customer_service.expense(25)   
+    
+    expect(customer_service.expenses).to eq(125)
   end
 
 end
