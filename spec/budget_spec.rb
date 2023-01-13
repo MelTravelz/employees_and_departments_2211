96af651493fe1eeecb7dde328a8eb07dd7c95d3f
Melony Franchini <meltravelz@gmail.com>
2023-01-13 10:09:56 -0700

    test: Add #all_salaries

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -49,7 +49,36 @@ RSpec.describe Budget do
     health_service.expense("$500")
 
     expect(budget.low_expense_departments).to eq([customer_service, social_service])
-end
+  end
+
+  it "can list #add_salaries employees" do
+    budget = Budget.new("2022") 
+      
+    customer_service = Department.new("Customer Service")   
+    law_service = Department.new("Law Service")  
+
+    budget.add_department(customer_service)
+    budget.add_department(law_service)
+    
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "$90000"})  
+    ben = Employee.new({name: "Ben Matlock", age: "60", salary: "$250000"})
+    saul = Employee.new({name: "Saul Goodman", age: "48", salary: "$200000"}) 
+
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron) 
+    law_service.hire(ben)
+    law_service.hire(saul) 
+
+    expected = {
+      bobbi => 100000,
+      aaron => 90000,
+      ben => 250000,
+      saul => 200000
+    }
+    expect(budget.all_salaries).to eq(expected)
+    # expect(budget.all_salaries).to eq([100000, 90000, 250000, 200000])
+  end
 
 
 end
