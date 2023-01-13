6639f26064886c21498c0ec850951688c7cd8cb8
Melony Franchini <meltravelz@gmail.com>
2023-01-13 09:04:40 -0700

    test: Add exists & attributes

---
+++ spec/employee_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require './lib/employee'
+
+RSpec.describe Employee do
+
+    it 'exists' do
+      bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+
+      expect(bobbi).to be_instance_of(Employee)
+    end
+
+		it "has atrributes" do
+      bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+
+			expect(bobbi.name).to eq("Bobbi Jaeger")
+      expect(bobbi.age).to eq(30)
+      expect(bobbi.salary).to eq(100000)
+		end
+  
+
+end
